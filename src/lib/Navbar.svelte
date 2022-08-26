68479251850bdefc825b38829af67b063aa23bc6
Kevin Bahner <kbahner@kent.edu>
2022-08-26 14:58:41 -0400

    update 1

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Work from "./Work.svelte";
+	import Tools from "./Tools.svelte";
+    import Volunteer from "./Volunteer.svelte"
+
+    export let menu = 1;
+</script>
+
+<div id="Introduction">
+    
+    <div>
+        <h1>Kevin Bahner</h1>
+        <h4> <a href = "mailto: kevbahner@gmail.com">kevbahner@gmail.com</a> | <a href="https://www.linkedin.com/in/kevin-bahner-b313641a2/">Visit my LinkedIn!</h4>
+            <hr>
+        </div>
+        <div id="navbar">
+            <ul id="menu">
+                <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Work Expirence</a></li> 
+                <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Tooling Expirence</a></li>
+                <li><a href="/" on:click|preventDefault={() => (menu = 3)}>Volunteer Activities</a></li>
+            </ul>
+        </div>
+        <hr>
+</div>
+
+
+{#if menu === 1}
+<Work />
+{:else if menu === 2}
+<Tools />
+{:else if menu === 3}
+<Volunteer />
+{:else}
+<h1>
+	Page Not Found
+</h1>
+{/if}
+
+
+
+<style>
+a, a:hover, a:active, a:visited {
+     color: white;
+ }
+#navbar {
+    margin-left: 8%;
+    margin-right:8%;
+    margin-top: 0;
+    margin-bottom: 0;
+
+}
+#Introduction {
+    background-color:rgba(41, 41, 41, 0.679);
+    padding: 1%;
+}
+ul#menu li{
+		display : inline;
+	}
+#menu a {
+    text-decoration: none;
+}
+
+#menu {
+    width: 100%;
+    display: flex;
+    padding: 0;
+    list-style: none;
+    justify-content:space-between;
+    text-align: center;
+    
+}
+
+</style>
---
+++ src/lib/Tools.svelte
@@ -0,0 +1,18 @@
+<div>
+
+</div>
+
+
+
+<div>
+    <a class="infohead">Hard Skills:</a>
+    <li>Windows troubleshooting</li>
+    <li>Powershell Scripting</li>
+    <ul>Hardware repair</ul>
+    <ul>Jira Service Desk</ul>
+    <ul>Cisco Meraki</ul>
+    <ul>Familiar with MS MDT, WDS</ul>
+    <ul>Office Standup</ul>
+    <ul>General Networking</ul>
+    <ul>Teamviewer Management</ul>
+</div>
