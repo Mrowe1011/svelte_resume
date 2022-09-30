f372189fe126e1cbf55f6aac25984bb0f7cdc7d5
Kevin Bahner <kbahner@kent.edu>
2022-09-30 12:18:45 -0400

    add certification

--- src/lib/Education.svelte
+++ src/lib/Education.svelte
@@ -15,6 +15,13 @@
     </div>
     <hr in:fly="{{ x: 200, duration: 550 }}">
     <div in:fly="{{ x: 200, duration: 600 }}">
+        <h1>Google Cloud Digital Leader Certification</h1>
+        <h2>Issued Sep 2022 - Expires Sep 2025</h2>
+        <h3>Credential ID: <a href="https://www.credential.net/816a074b-3494-4f89-9b32-19b874c7ecb5">58098342</a></h3>
+
+    </div>
+    <hr in:fly="{{ x: 200, duration: 650 }}">
+    <div in:fly="{{ x: 200, duration: 700 }}">
         <h1>Riverside Highschool</h1>
         <h2>Lakeland College Credit Plus and Honors Classes | Honors Diploma | 3.6 GPA</h2>
 
@@ -34,5 +41,8 @@
     background-color:rgba(41, 41, 41, 0.679);
     padding: 1%;
     }
+    a, a:hover, a:active, a:visited {
+     color: white;
+ }
 
 </style>
